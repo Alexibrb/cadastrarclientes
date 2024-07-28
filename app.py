baa6b1270a4799ab3bf34ae185af5fe8b8e8fa1e
Alexibrb <alexandro.ibrb@gmail.com>
2024-07-27 21:19:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -105,11 +105,11 @@ with tab1:
                 st.success("# Cadastro Efetuado com sucesso!!!!")
 
                 st.table(data2.tail(1))
-		    
-		btn_confirmar = st.button("Confirmar Dados")
-	
-		if btn_confirmar:   
-			st.experimental_rerun()
+	    btn_confirmar = st.button("Confirmar Dados")
+
+            	
+	    if btn_confirmar:   
+		st.experimental_rerun()
 
 
 
@@ -181,10 +181,11 @@ with tab1:
                 st.success("# Cadastro Efetuado com sucesso!!!!")
                 
                 st.table(data2.tail(1))
-		btn_confirmar2 = st.button("Confirmar Dados")
-	
-		if btn_confirmar2:   
-			st.experimental_rerun()
+	    btn_confirmar = st.button("Confirmar Dados")
+
+            	
+	    if btn_confirmar:   
+		st.experimental_rerun()
 with tab2:
     visualisar()
 with tab3:
