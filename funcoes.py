951f8e15f4c764283ac0d4d59bfad9c0447adeac
Alexibrb <alexandro.ibrb@gmail.com>
2024-07-27 22:06:35 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -63,20 +63,7 @@ def estilos():
 		visibility: hidden;
 
 	 }
-  	#footer {
-	
-	visibility: hidden;
-	
-	}
-	#footer:after {
-		content:'goodbye'; 
-		visibility: visible;
-		display: block;
-		position: relative;
-		#background-color: red;
-		padding: 5px;
-		top: 2px;
-	}
+  	
        
 
 	
