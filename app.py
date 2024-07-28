aba8d580e59c777c42697cb3c689742cddb15385
Alexibrb <alexandro.ibrb@gmail.com>
2024-07-27 21:46:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.markdown("""<style>
 		width:200px;
         	white-space: pre-wrap;
 		background-color: #2e5db7; 
-		border-radius:50px 50px 0px 0px;
+		border-radius:30px 30px 0px 0px;
 		gap: 3px;
 		padding-top: 5px;
 		padding-bottom: 5px;
