c09951c472bf2f553f1c08adcb934e14059dbf45
Alexibrb <alexandro.ibrb@gmail.com>
2024-08-06 22:29:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,46 +30,110 @@ with tab1:
 	if os.path.exists('clientes.csv'):
         	tabclientes = pd.read_csv('clientes.csv', sep=",")
 
-		with st.form("configp", clear_on_submit=True):
+	with st.form("configp", clear_on_submit=True):
+		cidade = st.selectbox(label="Selecione a sua Cidade:", options=[
+		"Clique aqui",
+		"Condeúba",
+		"Maetinga",
+		"Pres. Jânio Quadros",
+		"Cordeiros",
+		"Piripá",
+		"Mortugaba"
+	    ])
+		nome = st.text_input("Digite seu nome", placeholder="Nome Completo")
+		telefone = st.text_input("Digite seu Telefone", placeholder="(xx) xxxx-xxxx")
+		rg = st.text_input("Digite seu RG", placeholder="xxxxxxxx-xx")
+		cpf = st.text_input("Digite seu CPF", placeholder="xxx.xxx.xxx-xx")
+		endereco_obra = st.text_input("Digite o Endereço da Obra", placeholder="Rua, nº e bairro")
+		endereco_resid = st.text_input("Digite o Endereço Residencial", placeholder="Rua, nº e bairro")
+		obs = st.text_area("Observação", placeholder="não obrigatório")
+		# Mensagem de aviso
+		warning_message = st.empty()
+
+		# Desativando o botão se algum campo estiver vazio
+		if not are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
+			warning_message.warning("Por favor, preencha todos os campos. ⤴️")
+		 else:
+			warning_message.empty()
+
+		btn_cadastro = st.form_submit_button("Cadastrar Dados")
+
+
+		if btn_cadastro and are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
+			cidadec = cidade
+			cliente = nome
+			fone = telefone
+			rgc = rg
+			cpfc = cpf
+			end1 = endereco_obra
+			end2 = endereco_resid
+			obs1 = obs
+			data2 = pd.DataFrame(tabclientes)
+			d = {"Cidade": cidadec,
+			     "Nome": cliente,
+			     "Telefone": fone,
+			     "RG": rgc,
+			     "CPF": cpfc,
+			     "Endereço_Obra": end1,
+			     "Endereço_Residencial": end2,
+			     "Observação": obs1
+			     }
+			df2 = pd.DataFrame(d, index=[0])
+			data2 = pd.concat([data2, df2], ignore_index=True)
+			data2.to_csv('clientes.csv', index=False)
+			placeholder.empty()  # Limpa o conteúdo anterior
+			update_content()  # Atualiza o conteúdo
+			st.table(data2.tail(1))
+	else:
+
+		with st.form("config", clear_on_submit=True):
 			cidade = st.selectbox(label="Selecione a sua Cidade:", options=[
-	                "Clique aqui",
-	                "Condeúba",
-	                "Maetinga",
-	                "Pres. Jânio Quadros",
-	                "Cordeiros",
-	                "Piripá",
-	                "Mortugaba"
-	            ])
-	            	nome = st.text_input("Digite seu nome", placeholder="Nome Completo")
-	            	telefone = st.text_input("Digite seu Telefone", placeholder="(xx) xxxx-xxxx")
-	            	rg = st.text_input("Digite seu RG", placeholder="xxxxxxxx-xx")
-	            	cpf = st.text_input("Digite seu CPF", placeholder="xxx.xxx.xxx-xx")
-	            	endereco_obra = st.text_input("Digite o Endereço da Obra", placeholder="Rua, nº e bairro")
-	            	endereco_resid = st.text_input("Digite o Endereço Residencial", placeholder="Rua, nº e bairro")
-	            	obs = st.text_area("Observação", placeholder="não obrigatório")
-	            	# Mensagem de aviso
-	            	warning_message = st.empty()
+			"Clique aqui",
+			"Condeúba",
+			"Maetinga",
+			"Pres. Jânio Quadros",
+			"Cordeiros",
+			"Piripá",
+			"Mortugaba"
+		    ])
+			nome = st.text_input("Digite seu nome", placeholder="Nome Completo")
+			 telefone = st.text_input("Digite seu Telefone", placeholder="(xx) xxxx-xxxx")
+			 rg = st.text_input("Digite seu RG", placeholder="xxxxxxxx-xx")
+			 cpf = st.text_input("Digite seu CPF", placeholder="xxx.xxx.xxx-xx")
+			 endereco_obra = st.text_input("Digite o Endereço da Obra", placeholder="Rua, nº e bairro")
+			 endereco_resid = st.text_input("Digite o Endereço Residencial", placeholder="Rua, nº e bairro")
+			 obs = st.text_area("Observação", placeholder="não obrigatório")
+			# Mensagem de aviso
+			warning_message = st.empty()
 	
-	            	# Desativando o botão se algum campo estiver vazio
-	            	if not are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
-	               		warning_message.warning("Por favor, preencha todos os campos. ⤴️")
-	           	 else:
-	               	 	warning_message.empty()
+			 # Desativando o botão se algum campo estiver vazio
+			if not are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
+				warning_message.warning("Por favor, preencha todos os campos. ⤴️")
+			else:
+				warning_message.empty()
 	
-	            	btn_cadastro = st.form_submit_button("Cadastrar Dados")
-	
+			btn_cadastro = st.form_submit_button("Cadastrar Dados")
 	
 			if btn_cadastro and are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
 				cidadec = cidade
 				cliente = nome
-	                	fone = telefone
-	                	rgc = rg
-	                	cpfc = cpf
-	                	end1 = endereco_obra
-	                	end2 = endereco_resid
-	                	obs1 = obs
-	                	data2 = pd.DataFrame(tabclientes)
-	                	d = {"Cidade": cidadec,
+				fone = telefone
+				rgc = rg
+				cpfc = cpf
+				end1 = endereco_obra
+				end2 = endereco_resid
+				obs1 = obs
+		
+				data2 = pd.DataFrame(columns=['Cidade',
+							      'Nome',
+							      'Telefone',
+							      'RG',
+							      'CPF',
+							      'Endereço_Obra',
+							      'Endereço_Residencial',
+							      'Observação'
+							      ])
+				d = {"Cidade": cidadec,
 				     "Nome": cliente,
 				     "Telefone": fone,
 				     "RG": rgc,
@@ -78,77 +142,13 @@ with tab1:
 				     "Endereço_Residencial": end2,
 				     "Observação": obs1
 				     }
+		
 				df2 = pd.DataFrame(d, index=[0])
 				data2 = pd.concat([data2, df2], ignore_index=True)
 				data2.to_csv('clientes.csv', index=False)
 				placeholder.empty()  # Limpa o conteúdo anterior
 				update_content()  # Atualiza o conteúdo
-	        		st.table(data2.tail(1))
-    		else:
-
-		        with st.form("config", clear_on_submit=True):
-		        	cidade = st.selectbox(label="Selecione a sua Cidade:", options=[
-		                "Clique aqui",
-		                "Condeúba",
-		                "Maetinga",
-		                "Pres. Jânio Quadros",
-		                "Cordeiros",
-		                "Piripá",
-		                "Mortugaba"
-		            ])
-		            	nome = st.text_input("Digite seu nome", placeholder="Nome Completo")
-		           	 telefone = st.text_input("Digite seu Telefone", placeholder="(xx) xxxx-xxxx")
-		           	 rg = st.text_input("Digite seu RG", placeholder="xxxxxxxx-xx")
-		           	 cpf = st.text_input("Digite seu CPF", placeholder="xxx.xxx.xxx-xx")
-		           	 endereco_obra = st.text_input("Digite o Endereço da Obra", placeholder="Rua, nº e bairro")
-		           	 endereco_resid = st.text_input("Digite o Endereço Residencial", placeholder="Rua, nº e bairro")
-		           	 obs = st.text_area("Observação", placeholder="não obrigatório")
-		            	# Mensagem de aviso
-		            	warning_message = st.empty()
-		
-		           	 # Desativando o botão se algum campo estiver vazio
-		            	if not are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
-		               		warning_message.warning("Por favor, preencha todos os campos. ⤴️")
-		            	else:
-		                	warning_message.empty()
-		
-		            	btn_cadastro = st.form_submit_button("Cadastrar Dados")
-		
-				if btn_cadastro and are_fields_filled(nome, telefone, rg, cpf, endereco_obra, endereco_resid):
-					cidadec = cidade
-			                cliente = nome
-			                fone = telefone
-			                rgc = rg
-			                cpfc = cpf
-			                end1 = endereco_obra
-			                end2 = endereco_resid
-			                obs1 = obs
-			
-			                data2 = pd.DataFrame(columns=['Cidade',
-			                                              'Nome',
-			                                              'Telefone',
-			                                              'RG',
-			                                              'CPF',
-			                                              'Endereço_Obra',
-			                                              'Endereço_Residencial',
-			                                              'Observação'
-			                                              ])
-			                d = {"Cidade": cidadec,
-			                     "Nome": cliente,
-			                     "Telefone": fone,
-			                     "RG": rgc,
-			                     "CPF": cpfc,
-			                     "Endereço_Obra": end1,
-			                     "Endereço_Residencial": end2,
-			                     "Observação": obs1
-			                     }
-			
-			                df2 = pd.DataFrame(d, index=[0])
-			                data2 = pd.concat([data2, df2], ignore_index=True)
-			                data2.to_csv('clientes.csv', index=False)
-			                placeholder.empty()  # Limpa o conteúdo anterior
-			    		update_content()  # Atualiza o conteúdo
-			                st.table(data2.tail(1))
+				st.table(data2.tail(1))
 ########################################### VISUALIZAR DADOS ##############################################	 
 with tab2:
 	st.markdown("### 📢 :green[VISUALIZAR DADOS]")
